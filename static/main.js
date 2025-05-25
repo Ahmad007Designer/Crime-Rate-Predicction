e688a146391cbc78567928d7574cd8a7aff8d758
Ahmad007Designer <ahmadmohdrock007@gmail.com>
2025-05-25 19:18:45 +0530

    some changes

--- static/main.js
+++ static/main.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     //Reference the DropDownList.
     var year = document.getElementById("year-dropdown");
-    for (var i = 2000; i <= 2025; i++) {
+    for (var i = 2000; i <= 2050; i++) {
         var option = document.createElement("OPTION");
         option.innerHTML = i;
         option.value = i;
